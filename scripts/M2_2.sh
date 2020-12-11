aab26808f8da8c1b243866cb1b6cd96020caec32
Sayam Choudhary <34365122+sayam47@users.noreply.github.com>
2020-12-11 19:52:44 +0530

    Create M2_2.sh

---
+++ scripts/M2_2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=40
+#SBATCH --job-name=M3_BM
+#SBATCH --error=M3_BM.err
+#SBATCH --output=M3_BM.out
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+
+
+module load cuda/10.1
+source /home/sayam.choudhary.cse17.iitbhu/HNAS-SR-master/src/env/bin/activate
+epochs=2101
+plot_every=1
+print_every=1
+debug=0
+batch_size=16
+n_threads=16
+train_data_path="/home/sayam.choudhary.cse17.iitbhu/sr_df2k"
+test_data_path="/home/sayam.choudhary.cse17.iitbhu/Set5"
+output_path="/scratch/sayam.choudhary.cse17.iitbhu/output_M3_2"
+
+python3 train_5.py --train_data_path "$train_data_path" --test_data_path "$test_data_path" --epochs $epochs --plot_every $plot_every --print_every $print_every --n_threads $n_threads --output_path "$output_path" --batch_size $batch_size --debug $debug --output_size 256x256 --warmup_batches 0 --method M3 --gan RAGAN --vgg_criterion L1 --weight_gan 0.001 --weight_vgg 1 --weight_hv 0.005 --max_psnr 35.5 --betas 0.9,0.999
